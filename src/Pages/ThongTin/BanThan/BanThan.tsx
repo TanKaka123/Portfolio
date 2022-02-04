b61a9cbb0686479de542b284f36ac1bb0ccdf5a9
Hong-Tan <hongtan1422002@sgmail.com>
2022-02-05 01:10:34 +0700

    customize content text

--- src/Pages/ThongTin/BanThan/BanThan.tsx
+++ src/Pages/ThongTin/BanThan/BanThan.tsx
@@ -14,7 +14,7 @@ function BanThan() {
                <div className="aboutme-role"><p className="">Vai trò </p>  <h3 className="">Lớp trưởng</h3></div>
                <div className="aboutme-major"><p className="">Chuyên ngành </p>  <h3 className="">Công nghệ thông tin</h3></div>
               
-               <div className="aboutme-university"><p className="">Trường </p>  <h3 className="">Trường ĐH Khoa Học Tự Nhiên-ĐHQGHCM</h3></div>
+               <div className="aboutme-university"><p className="">Trường </p>  <h3 className="">Trường ĐH Khoa Học Tự Nhiên - ĐHQGHCM</h3></div>
                <div className="aboutme-hobby"><p className="">Sở thích  </p>  <h3 className="">Code, bóng chuyền, bóng đá, bóng rỗ, chạy bộ,...</h3></div>
                <div className="aboutme-idol"><p className="">Thần tượng  </p>  <h3 className="">Abraham Lincoln, Ricardo Kaká</h3></div>
                <div className="aboutme-quetos"><p className="">Quetos </p>  <h3 className="">"Cách tốt nhất để dự đoán tương lai là tạo ra nó" - Abraham Lincoln</h3></div>
