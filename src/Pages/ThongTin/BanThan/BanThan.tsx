485bb1e3debee648bcf8b1e0e8f42c669d017e8e
Hong-Tan <hongtan1422002@sgmail.com>
2022-02-11 14:31:22 +0700

    customize thong tin

--- src/Pages/ThongTin/BanThan/BanThan.tsx
+++ src/Pages/ThongTin/BanThan/BanThan.tsx
@@ -8,16 +8,16 @@ function BanThan() {
             <img src="./avatar-HongTan.png" alt="Tan" className="aboutme-avatar" />
             </div>
            <div className="aboutme-desc">
-               <div className="aboutme-name"> <p className="">Họ và Tên </p>  <h3 className="">Nguyễn Hồng Tấn</h3></div>
-               <div className="aboutme-date"> <p className="">N/T/N sinh </p>  <h3 className="">14/02/2002</h3></div>
-               <div className="aboutme-job"><p className="">Nghề nghiệp   </p>  <h3 className="">Sinh viên</h3></div>
-               <div className="aboutme-role"><p className="">Vai trò </p>  <h3 className="">Lớp trưởng</h3></div>
-               <div className="aboutme-major"><p className="">Chuyên ngành </p>  <h3 className="">Công nghệ thông tin</h3></div>
+               <div className="aboutme-name"> <p className="">Họ và Tên </p>  <h4 className="">Nguyễn Hồng Tấn</h4></div>
+               <div className="aboutme-date"> <p className="">N/T/N sinh </p>  <h4 className="">14/02/2002</h4></div>
+               <div className="aboutme-job"><p className="">Nghề nghiệp   </p>  <h4 className="">Sinh viên</h4></div>
+               <div className="aboutme-role"><p className="">Vai trò </p>  <h4 className="">Lớp trưởng</h4></div>
+               <div className="aboutme-major"><p className="">Chuyên ngành </p>  <h4 className="">Công nghệ thông tin</h4></div>
               
-               <div className="aboutme-university"><p className="">Trường </p>  <h3 className="">Trường ĐH Khoa Học Tự Nhiên - ĐHQGHCM</h3></div>
-               <div className="aboutme-hobby"><p className="">Sở thích  </p>  <h3 className="">Code, bóng chuyền, bóng đá, bóng rỗ, chạy bộ,...</h3></div>
-               <div className="aboutme-idol"><p className="">Thần tượng  </p>  <h3 className="">Abraham Lincoln, Ricardo Kaká</h3></div>
-               <div className="aboutme-quetos"><p className="">Quetos </p>  <h3 className="">"Cách tốt nhất để dự đoán tương lai là tạo ra nó" - Abraham Lincoln</h3></div>
+               <div className="aboutme-university"><p className="">Trường </p>  <h4 className="">Trường ĐH Khoa Học Tự Nhiên - ĐHQGHCM</h4></div>
+               <div className="aboutme-hobby"><p className="">Sở thích  </p>  <h4 className="">Code, bóng chuyền, bóng đá, bóng rỗ, chạy bộ,...</h4></div>
+               <div className="aboutme-idol"><p className="">Thần tượng  </p>  <h4 className="">Abraham Lincoln, Ricardo Kaká</h4></div>
+               <div className="aboutme-quetos"><p className="">Quetos </p>  <h4 className="">"Cách tốt nhất để dự đoán tương lai là tạo ra nó" - Abraham Lincoln</h4></div>
                <MoveTop/>
            </div>
         </div>
